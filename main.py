e6afbcef7fa8c5a0d09d51934ca25af47a6232d1
ongweiii <ongweilovespjc@hotmail.com>
2026-09-09 17:56:00 +0800

    created pipeline

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.tools.tools import web_search, scrape_url
+from src.pipeline.pipeline import run_research_pipeline
 
-res = scrape_url.invoke("https://www.artificialintelligence-news.com/")
-print(res)
+topic = "the impact of AI on the job market in 2026"
+
+run_research_pipeline(topic)
